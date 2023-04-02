2904c61ac0ea5adc302b5f20a25d9546a406420f
Eric Manning <emanning1030@gmail.com>
2023-04-02 17:32:45 -0400

    Counters done, winscreen done, reformatted to look better

--- Escape.py
+++ Escape.py
@@ -1,8 +1,8 @@
 import curses
 from curses import wrapper
 import queue
-import time
 import copy
+import time
 
 levels = [
     [
@@ -24,34 +24,50 @@ levels = [
     ["#", " ", " ", " ", " ", " ", " ", "#"],
     ["#", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "#"]
+    ],
+    [
+    ["#", "#", "#", "#", "#"],
+    ["#", " ", "O", " ", "X"],
+    ["#", "!", " ", " ", "#"],
+    ["#", "#", "#", "#", "#"]
+    ],
+    [
+    ["#", "#", "#", "#", "#"],
+    ["#", " ", " ", "!", "#"],
+    ["X", " ", "O", " ", "#"],
+    ["#", "#", "#", "#", "#"]
     ]
 ]
 
-def refreshLevel(level, stdscr, path):
+def refreshLevel(level, stdscr, levelNum, moves, path):
     stdscr.clear()
-    printLevel(level, stdscr)
+    printLevel(level, stdscr, levelNum, moves)
     # path debug
-    printLevel(level, stdscr, path)
+    # printLevel(level, stdscr, levelNum, moves, path)
     stdscr.refresh()
 
-def printLevel(level, stdscr, path = []):
+def printLevel(level, stdscr, levelNum, moves, path = []):
     cyan = curses.color_pair(1)
     magenta = curses.color_pair(2)
     red = curses.color_pair(3)
     white = curses.color_pair(4)
     for i, row in enumerate(level):
         for j, value in enumerate(row):
+            iPrintPos, jPrintPos = i + 1, (j + 1) * 2
             if path != None: 
                 if (i, j) in path:
-                    stdscr.addstr(i, j * 2, "X", magenta)
+                    stdscr.addstr(iPrintPos, jPrintPos, "X", magenta)
             if value == "#":
-                stdscr.addstr(i, j * 2, value, white)
+                stdscr.addstr(iPrintPos, jPrintPos, value, white)
             elif value == "X":
-                stdscr.addstr(i, j * 2, value, red)
+                stdscr.addstr(iPrintPos, jPrintPos, value, red)
             elif value == "O":
-                stdscr.addstr(i, j * 2, value, cyan)
+                stdscr.addstr(iPrintPos, jPrintPos, value, cyan)
             elif value == "!":
-                stdscr.addstr(i, j * 2, value, magenta)
+                stdscr.addstr(iPrintPos, jPrintPos, value, magenta)
+    currentCol = len(level[0]) * 2 + 1
+    stdscr.addstr(1, currentCol + 1, "level " + str(levelNum), white)
+    stdscr.addstr(2, currentCol + 1, "total moves " + str(moves), white)
 
 def findLocation(level, character):
     for i, row in enumerate(level):
@@ -140,40 +156,65 @@ def checkLose(level, player):
                 return False
     return True
 
+def loseWindow(stdscr):
+    magenta = curses.color_pair(2)
+    stdscr.clear()
+    stdscr.addstr(1, 6, "you lost!", magenta)
+    stdscr.addstr(2, 6, "press space to play again", magenta)
+    stdscr.refresh()
+
+def winWindow(stdscr, movesList):
+    magenta = curses.color_pair(2)
+    stdscr.clear()
+    stdscr.addstr(1, 6, "you escaped the final room!", magenta)
+    stdscr.refresh()
+    row = 3
+    for i, moves in enumerate(movesList):
+        time.sleep(0.5)
+        levelString = f"Level {str(i + 1)}: {str(moves).zfill(3)}"
+        if i % 2 == 0:
+            stdscr.addstr(row, 6, levelString, magenta)
+        if i % 2 == 1:
+            stdscr.addstr(row, 21, levelString, magenta)
+            row += 1
+        stdscr.refresh()
+    if len(movesList) % 2 == 1:
+        row += 1
+    row += 1
+    time.sleep(0.5)
+    stdscr.addstr(row, 6, "press space to play again", magenta)
+    stdscr.refresh()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_RED)
     curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    magenta = curses.color_pair(2)
 
     levelIndex = 0
     enemy = "!"
     player = "O"
+    movesList = []
     while True:
+        moves = 0
         if levelIndex <= len(levels) - 1:
             level = copy.deepcopy(levels[levelIndex])
         else:
-            stdscr.clear()
-            stdscr.addstr(3, 6, "you escaped the final room!", magenta)
-            stdscr.addstr(4, 6, "press space to play again", magenta)
-            stdscr.refresh()
+            winWindow(stdscr, movesList)
             while True:
                 key = stdscr.getch()
                 if key == ord(" "):
                     levelIndex = 0
+                    movesList = []
                     break
             continue
         while True:
             path = findPath(level, enemy, player)
-            refreshLevel(level, stdscr, path)
+            refreshLevel(level, stdscr, levelIndex + 1, moves, path)
 
             lost = checkLose(level, player)
             if lost:
-                stdscr.clear()
-                stdscr.addstr(3, 6, "you lost", magenta)
-                stdscr.addstr(4, 6, "press space to play again", magenta)
-                stdscr.refresh()
+                loseWindow(stdscr)
                 while True:
                     key = stdscr.getch()
                     if key == ord(" "):
@@ -198,9 +239,12 @@ def main(stdscr):
                         continue
                     elif goodMove == True:
                         moveEnemy(level, path, enemy)
+                        moves += 1
                         break
                     elif goodMove == "escape":
                         levelIndex += 1
+                        moves += 1
+                        movesList.append(moves)
                         break
                 if goodMove == "escape":
                     break
